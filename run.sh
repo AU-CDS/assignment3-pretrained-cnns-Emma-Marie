4af957dc8e64144ee29b53af08e7cb9067ebc39b
Emma-Marie <emma-marie@outlook.dk>
2023-04-24 18:58:00 +0200

    script works on test

---
+++ run.sh
@@ -0,0 +1,8 @@
+#activate the venv
+source ./tensorflow_env/bin/activate
+
+#run the code
+python3 src/clf_pretrainedCnn
+
+#deactivate the venv
+deactivate
--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+
+#create virtual environment
+python3 -m venv tensorflow_env
+
+source ./tensorflow_env/bin/activate
+
 #then install requirements.txt
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+#deactivate the venv
+deactivate
