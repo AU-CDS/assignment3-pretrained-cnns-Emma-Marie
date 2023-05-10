ebd50a04af8cb164d97188c7f79ab42e53eb9fae
Emma-Marie <emma-marie@outlook.dk>
2023-05-10 09:51:35 +0200

    script worked on

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 source ./tensorflow_env/bin/activate
 
 #run the code
-python3 src/clf_pretrainedCnn
+python3 src/clf_pretrainedCnn.py
 
 #deactivate the venv
 deactivate
--- src/clf_pretrainedCnn.py
+++ src/clf_pretrainedCnn.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 # tf tools
 import tensorflow as tf
 # image processsing
@@ -14,6 +15,8 @@ from tensorflow.keras.layers import (Flatten,
                                      Dense, 
                                      Dropout, 
                                      BatchNormalization)
+
+from tensorflow.keras import layers
 # generic model object
 from tensorflow.keras.models import Model
 # optimizers
@@ -26,18 +29,26 @@ from sklearn.metrics import classification_report
 import numpy as np
 import matplotlib.pyplot as plt
 # for json files
-import pandas as pd
-import json
+import json # necessary?
+
 # utils plotting function
 import sys
 sys.path.append(".")
 import utils.plotting as pl
 
+
 def get_data():
     # Load the json metadata (containing the labels)
-    test_df = pd.read_json(os.path.join("in", "images", "metadata","test_data.json" ), lines=True)
-    train_df = pd.read_json(os.path.join("in", "images", "metadata","train_data.json" ), lines=True)
-    val_df = pd.read_json(os.path.join("in", "images", "metadata","val_data.json" ), lines=True)
+    #test_df = pd.read_json(os.path.join("in", "images", "metadata","test_data.json" ), lines=True)
+    test_df = pd.read_json(os.path.join("..", "..", "431824", "images", "metadata", "test_data.json" ), lines=True)
+    #train_df = pd.read_json(os.path.join("in", "images", "metadata","train_data.json" ), lines=True)
+    train_df = pd.read_json(os.path.join("..", "..", "431824", "images", "metadata", "train_data.json" ), lines=True)
+    #val_df = pd.read_json(os.path.join("in", "images", "metadata","val_data.json" ), lines=True)
+    val_df = pd.read_json(os.path.join("..", "..", "431824", "images", "metadata", "val_data.json" ), lines=True)
+
+    #test_df = test_df.sample(n=1200)
+    #train_df = train_df.sample(n=4000)
+    #val_df = val_df.sample(n=800)
 
     # Generator train data
     train_datagen = ImageDataGenerator(horizontal_flip=True,
@@ -94,6 +105,8 @@ def get_data():
         shuffle = True,
         seed = 42,
     )
+    
+
     return train_images, test_images, val_images, test_df
 
 def train_classifier(train_images, test_images, val_images):
@@ -150,7 +163,8 @@ def main():
     history, model = train_classifier(train_images, test_images, val_images)
     # Training and validation history plots
     pl.plot_history(history, 10)
-    output_path = os.path.join("out", "train_and_val_plots.png")
+    #output_path = os.path.join("out", "train_and_val_plots.png")
+    output_path = os.path.join("out", "TEST_plots.png")
     plt.savefig(output_path, dpi = 100)
     print("Plot is saved!")
     # Predictions
@@ -165,7 +179,8 @@ def main():
     y_test = list(test_df.class_label)
     report = classification_report(y_test, pred)
     # Save report in "out"
-    file_path = os.path.join("out", "classification_report.txt")
+    #file_path = os.path.join("out", "classification_report.txt")
+    file_path = os.path.join("out", "TEST_rep.txt")
     with open(file_path, "w") as f: #"writing" classifier report and saving it
         f.write(report)
     print("Classification report is saved!")
